ccb87f33c8046b4eef96920734762892b1e29d95
Whilack <leonid.gainar@gmail.com>
2022-01-26 15:17:56 +0200

    Removed unused packages and store

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib/framework";
 
+import "roboto-fontface/css/roboto/roboto-fontface.css";
+import "@mdi/font/css/materialdesignicons.css";
+
 Vue.use(Vuetify);
 
 export default new Vuetify({});
